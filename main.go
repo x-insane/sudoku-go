a4ab5ac10c6f77b1449dca89174dce6400ab0d6b
x-insane <yanxuliu1997@163.com>
2023-02-20 01:12:55 +0800

    simply

--- main.go
+++ main.go
@@ -5,13 +5,6 @@ import (
 	"strings"
 )
 
-type Point struct {
-	X, Y   int
-	Value  int
-	Acc    float64
-	CanUse []int
-}
-
 func main() {
 	for i, input := range []string{
 		"000007020 712030690 080600050 200000100 050000060 008000009 040002080 063080274 020900000",
@@ -26,7 +19,7 @@ func main() {
 		}
 		for i := 0; i < 9; i++ {
 			for j := 0; j < 9; j++ {
-				fmt.Print(data[i][j].Value)
+				fmt.Print(data[i][j])
 			}
 			fmt.Println()
 		}
@@ -34,31 +27,26 @@ func main() {
 	}
 }
 
-func solve(input string) ([9][9]Point, bool) {
+func solve(input string) ([9][9]int, bool) {
 	lines := strings.Split(input, " ")
-	var data [9][9]Point
+	var data [9][9]int
 	var xUsed [9][10]bool
 	var yUsed [9][10]bool
 	var zUsed [9][10]bool
 	for i, line := range lines {
 		for j, char := range line {
-			data[i][j] = Point{
-				X:     i,
-				Y:     j,
-				Value: int(char - '0'),
-			}
-			if data[i][j].Value != 0 {
-				data[i][j].Acc = 1
-				xUsed[i][data[i][j].Value] = true
-				yUsed[j][data[i][j].Value] = true
-				zUsed[getZ(i, j)][data[i][j].Value] = true
+			data[i][j] = int(char - '0')
+			if data[i][j] != 0 {
+				xUsed[i][data[i][j]] = true
+				yUsed[j][data[i][j]] = true
+				zUsed[getZ(i, j)][data[i][j]] = true
 			}
 		}
 	}
 	return data, loop(&xUsed, &yUsed, &zUsed, &data, 0, 0)
 }
 
-func loop(xUsed, yUsed, zUsed *[9][10]bool, data *[9][9]Point, i, j int) bool {
+func loop(xUsed, yUsed, zUsed *[9][10]bool, data *[9][9]int, i, j int) bool {
 	loopNext := func() bool {
 		if i == 8 && j == 8 {
 			return true
@@ -70,7 +58,7 @@ func loop(xUsed, yUsed, zUsed *[9][10]bool, data *[9][9]Point, i, j int) bool {
 		}
 	}
 
-	if data[i][j].Value > 0 {
+	if data[i][j] > 0 {
 		return loopNext()
 	}
 
@@ -79,14 +67,14 @@ func loop(xUsed, yUsed, zUsed *[9][10]bool, data *[9][9]Point, i, j int) bool {
 		if xUsed[i][value] || yUsed[j][value] || zUsed[k][value] {
 			continue
 		}
-		data[i][j].Value = value
+		data[i][j] = value
 		xUsed[i][value] = true
 		yUsed[j][value] = true
 		zUsed[k][value] = true
 		if ok := loopNext(); ok {
 			return ok
 		}
-		data[i][j].Value = 0
+		data[i][j] = 0
 		xUsed[i][value] = false
 		yUsed[j][value] = false
 		zUsed[k][value] = false
